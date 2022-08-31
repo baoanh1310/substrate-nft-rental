4bb94d400e8eff6ee4caaad8de65e81c7cae62dc
Jade <107318290+Jade-Phan@users.noreply.github.com>
2022-08-31 10:57:10 +0700

    Update nft.rs

--- pallets/nft_currency/src/nft.rs
+++ pallets/nft_currency/src/nft.rs
@@ -1,13 +1,8 @@
 use frame_support::{dispatch::{result::Result, DispatchError, DispatchResult}};
 pub use sp_std::*;
-pub use frame_support::{pallet_prelude::Member, traits::Currency};
 use sp_std::vec::Vec;
 
 pub trait NonFungibleToken<AccountId>{
-	// type TokenId: Parameter+ Member +MaxEncodedLen+EncodeLike + Default+ Copy+Into<u64> ;
-	type Currency: Currency<AccountId>;
-	// type Administrator : EnsureOrigin<AccountId>;
-	// fn administrator() -> AccountId;
 	fn symbol() -> Vec<u8>;
 	fn get_name() -> Vec<u8>;
 	fn token_uri(token_id: Vec<u8>) -> Vec<u8>;
@@ -20,5 +15,4 @@ pub trait NonFungibleToken<AccountId>{
 	fn is_approve_for_all(account_approve:(AccountId,AccountId)) -> bool;
 	fn approve(from: AccountId, to: AccountId,token_id: Vec<u8>) -> DispatchResult;
 	fn set_approve_for_all(from: AccountId, to: AccountId) -> DispatchResult;
-
 }
